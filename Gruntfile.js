0e0579f86a494ed8420d340bdc31116b318fbb26
makotot <makoto.ttn@gmail.com>
2015-11-20 13:23:43 +0900

    init

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+
+  require('time-grunt')(grunt);
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+
+    path: {
+      src: './src',
+      dist: './dist'
+    },
+
+    clean: {
+      all: ['<%= path.dist %>']
+    },
+
+    copy: {
+      all: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= path.src %>/img/',
+            src: '**/*.*',
+            dest: '<%= path.dist %>/img/'
+          }
+        ]
+      }
+    },
+
+    sass: {
+      all: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= path.src %>/scss',
+            src: '*.scss',
+            dest: '<%= path.dist %>/css',
+            ext: '.css'
+          }
+        ]
+      }
+    },
+
+    postcss: {
+      options: {
+        sourcemap: true,
+        processors: [
+          require('postcss-sprites')({
+            stylesheetPath: '<%= path.dist %>/css',
+            spritePath: '<%= path.dist %>/img/sprites.png'
+          })
+        ]
+      },
+      dist: {
+        src: '<%= path.dist %>/css/index.css'
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['clean']);
+  grunt.registerTask('build', ['clean', 'copy', 'sass', 'postcss']);
+};
